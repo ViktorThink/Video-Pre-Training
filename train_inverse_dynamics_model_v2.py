f5d5b841cd8453d6f43d264d0a7965bc67c3b9c9
ViktorThink <viktor.bowallius@hotmail.com>
2022-10-05 12:39:09 +0200

    G

--- train_inverse_dynamics_model_v2.py
+++ train_inverse_dynamics_model_v2.py
@@ -163,7 +163,8 @@ def main(model, weights, video_path, json_path, n_batches, n_frames):
         predicted_actions = agent.predict_actions_training(frames)
         keys = [a for a in predicted_actions.keys()]
         if _ == 0:
-            print("predicted_actions keys",keys)
+            print("predicted_actions",keys)
+            print("predicted_actions",predicted_actions)
             print("\n\camera",predicted_actions["camera"].shape)
             print("\n\camera",predicted_actions["camera"][0])
             print("recorded_actions[0]",recorded_actions[0])
