e91506ae89c8c7dce90826808e7a32e500806b70
ViktorThink <viktor.bowallius@hotmail.com>
2022-10-11 12:41:27 +0200

    g

--- train_inverse_dynamics_model_v6.py
+++ train_inverse_dynamics_model_v6.py
@@ -303,6 +303,7 @@ def main(model, weights, video_path, json_path, n_batches, n_frames, accumulatio
             
             
             # print("=== Predicting actions ===")
+            print("batch_images",batch_images)
             pi_distribution = agent.predict_actions_training(batch_images)
             
             pi_camera=pi_distribution["camera"][0]
